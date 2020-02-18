dc7a9f79a729bbc1bc13f821b68e6c9fccb12eb2
demkantor <demkantor@gmail.com>
2020-02-18 16:02:53 -0600

    learning all aabout react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
+class App extends Component {
+  state = {
+    inputObject:{
+      user: '',
+      city: '',
+      state: '',
+    },
+    finalObject: {
+      user: '',
+      city: '',
+      state: ''
+    },
+    numberArray: [7,8,9],
+  }
+
+  // handleUserChange = (event) => {
+  //   console.log(event.target.value);
+  //   this.setState({
+  //     user: event.target.value
+  //   });
+  // }
+
+  // handleCityChange = (event) => {
+  //   this.setState({
+  //     city: event.target.value
+  //   });
+  // }
+  
+  //   handleStateChange = (event) => {
+  //     this.setState({
+  //       state: event.target.value
+  //   });
+  // }
+
+  handleChange = (event, typeOf) => {
+    console.log(event.target.value, typeOf);
+    this.setState({
+      [typeOf]: event.target.value
+    })
+  }
+  updateArray = () => {
+    console.log(this.state.numberArray);
+    let newNum = 7;
+    this.setState({
+      numberArray: [...this.state.numberArray, newNum]
+    })
+  }
+
+  handleSubmit = (event) => {
+    this.setState({
+      finalObject: {
+        user: this.state.user,
+        city: this.state.city,
+        state: this.state.state
+      }
+  });
+  
+}
+  render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+      <h1>This is my App!</h1>
+      <input placeholder="name" onChange={(event)=> this.handleChange(event, 'user')}/>
+      <input placeholder="city" onChange={(event)=> this.handleChange(event, 'city')}/>
+      <input placeholder="state" onChange={(event)=> this.handleChange(event, 'state')}/>
+      <button onClick={this.handleSubmit} >Submit</button>
+      <h2>hi! my name is {this.state.finalObject.user} </h2>
+      <h2>and i live in: {this.state.finalObject.city}, {this.state.finalObject.state} </h2>
+      <button onClick={this.updateArray}>update number array</button>
+      <h3>{this.state.numberArray}</h3>
+            </div>
+    );
+   }
 }
 
 export default App;
